70d3240614a7adc4323d3454c1054056757c4c21
valed-dm <dmvaled@gmail.com>
2022-12-16 07:42:46 +0300

    import export csv exercises done

--- Chapter_05/import_csv.sql
+++ Chapter_05/import_csv.sql
@@ -87,3 +87,38 @@ COPY (
      )
 TO '/Users/dmitrijvaledinskij/SQL/practical-sql-2-main/Chapter_05/us_counties_mil_export.txt'
 WITH (FORMAT CSV, HEADER, DELIMITER '|');
+
+-- exercises -------
+-- 1
+-- Write a WITH statement to include with COPY to handle the import of an imaginary text file 
+-- whose first couple of rows look like this:
+		-- id:movie:actor
+		-- 50:#Mission: Impossible#:Tom Cruise
+
+COPY movie_data
+FROM 'your path'
+WITH(FORMAT CSV, HEADER, DELIMITER ':')
+
+-- 2
+-- Using the table us_counties_pop_est_2019 you created and filled in this chapter, 
+-- export to a CSV file the 20 counties in the United States that had the most births. 
+-- Make sure you export only each county’s name, state, and number of births. 
+-- (Hint: births are totaled for each county in the column births_2019.)
+
+COPY (
+	SELECT county_name, state_name, births_2019
+	FROM us_counties_pop_est_2019
+	ORDER BY births_2019 DESC
+	LIMIT 30
+)
+TO '/Users/dmitrijvaledinskij/SQL/practical-sql-2-main/Chapter_05/us_counties_births_export.csv'
+WITH (FORMAT CSV, HEADER)
+
+-- 3
+-- Imagine you’re importing a file that contains a column with these values:
+-- 17519.668
+-- 20084.461
+-- 18976.335
+-- Will a column in your target table with data type numeric(3,8) work for these values? Why or why not?
+
+-- answer: - it wont't work, cause data type must be set as numeric(8,3) - i.e. in reverse order of values.
